a3b12c9047d61322a8498e886bfc715133d227b5
Frank Guns <frank@g-sharp.be>
2021-02-16 16:12:35 +0100

    added migrations for prod

--- Code/Training.Migrations.Production/Migrations/20210216151149_m002.Designer.cs
+++ Code/Training.Migrations.Production/Migrations/20210216151149_m002.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Training.Dal.Context;
 
 namespace Training.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210216151149_m002")]
+    partial class m002
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
